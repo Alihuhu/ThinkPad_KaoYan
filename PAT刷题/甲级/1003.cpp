deae27ac48ba30f160283ef11a9df6695ba367c1
Alihuhu <alihuhu@tom.com>
2019-07-07 21:42:34 +0800

    xiugai

---
+++ PAT刷题/甲级/1003.cpp
@@ -0,0 +1,20 @@
+#include<cstdio>
+#include<vector>
+using namespace std;
+const int INF = 100000000;
+struct Node{
+    int v,dis;//v¶¥µã£¬dis±ßÈ¨
+};
+int minlong,minval;
+vector<Node> Adj[505];
+int n;
+int d[MAXV];
+bool vis[MAXV]={false};
+void Dijkstra(int s)
+{
+    int
+}
+int main()
+{
+
+}
