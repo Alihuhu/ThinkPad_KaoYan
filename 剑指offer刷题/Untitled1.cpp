85d2a3491b7047eae5277c02a5b945463cdd1696
Alihuhu <alihuhu@tom.com>
2019-08-10 20:55:10 +0800

    2019/8/10

---
+++ 剑指offer刷题/Untitled1.cpp
@@ -0,0 +1,148 @@
+// 计分（此处是完全遍历，其实可以用bfs或者dfs加减枝降低复杂度，通过调整权重值，调整AI智能程度以及攻守风格）
+    for (int row = 0; row < kBoardSizeNum; row++)
+        for (int col = 0; col < kBoardSizeNum; col++)
+        {
+            // 空白点就算
+            if (row > 0 && col > 0 &&gameMapVec[row][col] == 0)
+            {
+                // 遍历周围八个方向
+                for (int y = -1; y <= 1; y++)
+                    for (int x = -1; x <= 1; x++)
+                    {
+                        // 重置
+                        personNum = 0;
+                        botNum = 0;
+                        emptyNum = 0;
+
+                        // 原坐标不算
+                        if (!(y == 0 && x == 0))
+                        {
+                            // 每个方向延伸4个子
+
+                            // 对玩家白子评分（正反两个方向）
+                            for (int i = 1; i <= 4; i++)
+                            {
+                                if (row + i * y > 0 && row + i * y < kBoardSizeNum &&
+                                    col + i * x > 0 && col + i * x < kBoardSizeNum &&
+                                    gameMapVec[row + i * y][col + i * x] == 1) // 玩家的子
+                                {
+                                    personNum++;
+                                }
+                                else if (row + i * y > 0 && row + i * y < kBoardSizeNum &&
+                                         col + i * x > 0 && col + i * x < kBoardSizeNum &&
+                                         gameMapVec[row + i * y][col + i * x] == 0) // 空白位
+                                {
+                                    emptyNum++;
+                                    break;
+                                }
+                                else            // 出边界
+                                    break;
+                            }
+
+                            for (int i = 1; i <= 4; i++)
+                            {
+                                if (row - i * y > 0 && row - i * y < kBoardSizeNum &&
+                                    col - i * x > 0 && col - i * x < kBoardSizeNum &&
+                                    gameMapVec[row - i * y][col - i * x] == 1) // 玩家的子
+                                {
+                                    personNum++;
+                                }
+                                else if (row - i * y > 0 && row - i * y < kBoardSizeNum &&
+                                         col - i * x > 0 && col - i * x < kBoardSizeNum &&
+                                         gameMapVec[row - i * y][col - i * x] == 0) // 空白位
+                                {
+                                    emptyNum++;
+                                    break;
+                                }
+                                else            // 出边界
+                                    break;
+                            }
+
+                            if (personNum == 1)                      // 杀二
+                                scoreMapVec[row][col] += 10;
+                            else if (personNum == 2)                 // 杀三
+                            {
+                                if (emptyNum == 1)
+                                    scoreMapVec[row][col] += 30;
+                                else if (emptyNum == 2)
+                                    scoreMapVec[row][col] += 40;
+                            }
+                            else if (personNum == 3)                 // 杀四
+                            {
+                                // 量变空位不一样，优先级不一样
+                                if (emptyNum == 1)
+                                    scoreMapVec[row][col] += 60;
+                                else if (emptyNum == 2)
+                                    scoreMapVec[row][col] += 110;
+                            }
+                            else if (personNum == 4)                 // 杀五
+                                scoreMapVec[row][col] += 10100;
+
+                            // 进行一次清空
+                            emptyNum = 0;
+
+                            // 对AI黑子评分
+                            for (int i = 1; i <= 4; i++)
+                            {
+                                if (row + i * y > 0 && row + i * y < kBoardSizeNum &&
+                                    col + i * x > 0 && col + i * x < kBoardSizeNum &&
+                                    gameMapVec[row + i * y][col + i * x] == 1) // 玩家的子
+                                {
+                                    botNum++;
+                                }
+                                else if (row + i * y > 0 && row + i * y < kBoardSizeNum &&
+                                         col + i * x > 0 && col + i * x < kBoardSizeNum &&
+                                         gameMapVec[row +i * y][col + i * x] == 0) // 空白位
+                                {
+                                    emptyNum++;
+                                    break;
+                                }
+                                else            // 出边界
+                                    break;
+                            }
+
+                            for (int i = 1; i <= 4; i++)
+                            {
+                                if (row - i * y > 0 && row - i * y < kBoardSizeNum &&
+                                    col - i * x > 0 && col - i * x < kBoardSizeNum &&
+                                    gameMapVec[row - i * y][col - i * x] == -1) // AI的子
+                                {
+                                    botNum++;
+                                }
+                                else if (row - i * y > 0 && row - i * y < kBoardSizeNum &&
+                                         col - i * x > 0 && col - i * x < kBoardSizeNum &&
+                                         gameMapVec[row - i * y][col - i * x] == 0) // 空白位
+                                {
+                                    emptyNum++;
+                                    break;
+                                }
+                                else            // 出边界
+                                    break;
+                            }
+
+                            if (botNum == 0)                      // 普通下子
+                                scoreMapVec[row][col] += 5;
+                            else if (botNum == 1)                 // 活二
+                                scoreMapVec[row][col] += 10;
+                            else if (botNum == 2)
+                            {
+                                if (emptyNum == 1)                // 死三
+                                    scoreMapVec[row][col] += 25;
+                                else if (emptyNum == 2)
+                                    scoreMapVec[row][col] += 50;  // 活三
+                            }
+                            else if (botNum == 3)
+                            {
+                                if (emptyNum == 1)                // 死四
+                                    scoreMapVec[row][col] += 55;
+                                else if (emptyNum == 2)
+                                    scoreMapVec[row][col] += 100; // 活四
+                            }
+                            else if (botNum >= 4)
+                                scoreMapVec[row][col] += 10000;   // 活五
+
+                        }
+                    }
+
+            }
+        }
